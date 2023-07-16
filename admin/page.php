4532e2ca66754f657d3eff4010f49a10f45be42a
Mohamed Ibrahim <mohamedibrahimabdulghani@gmail.com>
2023-07-16 14:29:37 +0300

    edit in  page

--- admin/page.php
+++ admin/page.php
@@ -2,20 +2,27 @@
 
 /**
  *  Categories => [ Mange | Edit | Update | Add | Insert | Delete | State ]
+ * Condition ? True : False
  */
 
-$do = "";
-if(isset($_GET["do"])):
-    $do = $_GET["do"];
-else:
-    $do = "Manage";
-endif;
+
+$do = isset($_GET["do"]) ? $_GET["do"] : "Manage";
+
+// $do = "";
+// if(isset($_GET["do"])):
+//     $do = $_GET["do"];
+// else:
+//     $do = "Manage";
+// endif;
+
+
 
 
 // If The Page Is Main Page
 if($do == "Manage"):
     echo " Welcome You Are In Mange Category Page " . "<br>";
-    echo "<a href='page.php?do=Add'>Add New Category</a>";
+    echo "<a href='page.php?do=Add'>Add New Category</a>" . "<br>";
+    echo "<a href='page.php?do=Insert'>Insert New Category</a>" . "<br>";
     elseif($do == "Add"):
         echo " Welcome You Are In Add Category Page " . "<br>";
         elseif($do == "Insert"):
